ad2edc755f6195c180ce771cb62b2a7a299647cb
GMoulidharan <moulidharansrv@gmail.com>
2024-09-24 16:09:52 +0530

    Used product.js file to generate all HTML

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,4 @@
-const products = [{
-    image: 'images/Products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price: 50090
-}, {
-    image: 'images/Products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 87
-    },
-    price: 250095
-}, {
-    image: 'images/Products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    price: 40099
-},{
-    image:'images/products/black-2-slot-toaster.jpg',
-    name:'2 Slot Toaster - Black',
-    rating:{
-        stars:5,
-        count:2197
-    },
-    price:1899
-}
 
-];
 
 
 let productsHTML='';
@@ -56,7 +22,7 @@ products.forEach((product) => {
                 </div>
 
                 <div class="product-price">
-                    ₹${(product.price / 100).toFixed(2)}
+                    ₹${(product.priceCents / 100).toFixed(2)}
                 </div>
 
                 <div class="product-quantity-container">
