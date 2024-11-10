fbd61bbf1b36f9572d50a5cb2598cdda15e684b4
GMoulidharan <moulidharansrv@gmail.com>
2024-11-10 17:57:49 +0530

    Learn error handling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,14 +8,21 @@ import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
 async function loadpage(){
+    try{
+        //throw 'error1';
+        await loadProductsFetch();
 
-    await loadProductsFetch();
-
-    const value = await new Promise((resolve) =>{
+        const value = await new Promise((resolve,reject) =>{
+        //throw 'error1';
         loadCart(() =>{
+            //reject('error3');
             resolve('value3');
         });
     });
+
+    } catch(error){
+        console.log("Unexpected error. Please try again later");
+    }
 
         renderCheckoutHeader();
         renderOrderSummary();
